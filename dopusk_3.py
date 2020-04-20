c6ee1e53ddc08a50732c665fc17ce2d32d42ff46
viktoria2610 <skorpionicha@mail.ru>
2020-04-20 22:58:04 +0300

    add script 3

--- dopusk_3.py
+++ dopusk_3.py
@@ -8,45 +8,47 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-ro = [[0.7, 0.2999, 0.0001],
-      [0.6, 0.2999, 0.1001],
+ro = [[0.7, 0.29, 0.01],
+      [0.5, 0.3333, 0.1667],
       [0, 0, 1]]
 
-t22 = 0
-t12 = (ro[2][0] * (1 + ro[0][0] + ro[0][1] + ro[0][2]) + ro[1][1] * (1 - ro[0][0]) + ro[1][2] * (1 - ro[0][0])) / (
-        1 - ro[0][0] - ro[1][1] + ro[1][1] * ro[0][0])
-t02 = (ro[0][0] + ro[0][1] * (1 + t12) + ro[0][2]) / (1 - ro[0][0])
+ro = np.array(ro)
+vectorB = np.array([1, 1, 1])
+current_matrix = -ro
+current_matrix[0][0] = 1 - ro[0][0]
+current_matrix[1][1] = 1 - ro[1][1]
+print(np.linalg.inv(current_matrix).dot(vectorB)[1])
 
-print("Среднее время достижения поглощения (теория) (0): ", + t02)
-print("Среднее время достижения поглощения (теория) (1): ", + t12)
 
-cur_state = 0
-T = 1000
-time0 = 0
-time1 = 0
-for j in range(0, T):
-    c = random.random()
-    if cur_state == 0:
-        if c < ro[0][0]:
-            cur_state = 0
-            time0 = time0 + 1
-        elif ro[0][0] < c < (ro[0][0] + ro[0][1]):
-            cur_state = 1
-            time1 = time1 + 1
+# Практика
+
+T = 1000000
+n = 100000
+time = 0
+for i in range(0, n):
+    cur_state = 1
+    for j in range(0, T):
+        c = random.random()
+        if cur_state == 0:
+            if c < ro[0][0]:
+                cur_state = 0
+                time = time + 1
+            elif ro[0][0] < c < (ro[0][0] + ro[0][1]):
+                cur_state = 1
+                time = time + 1
+            else:
+                cur_state = 2
+                break
+        elif cur_state == 1:
+            if c < ro[1][0]:
+                cur_state = 0
+                time = time + 1
+            elif ro[1][0] < c < (ro[1][0] + ro[1][1]):
+                cur_state = 1
+                time = time + 1
+            else:
+                cur_state = 2
+                break
         else:
-            cur_state = 2
             break
-    elif cur_state == 1:
-        if c < ro[1][0]:
-            cur_state = 0
-            time0 = time0 + 1
-        elif ro[1][0] < c < (ro[1][0] + ro[1][1]):
-            cur_state = 1
-            time1 = time1 + 1
-        else:
-            cur_state = 2
-            break
-    else:
-        break
-print("Практика (0): ", + (time0 / T))
-print("Практика (1): ", + (time1 / T))
+print("Практика (0): ", + (time / n))
